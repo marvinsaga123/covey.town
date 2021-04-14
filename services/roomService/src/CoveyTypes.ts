23b0881cbeaec03815a36b6340cf1c31d48fd9df
Marvin Sagastume <marvin@outcomes4me.com>
2021-04-13 23:57:34 -0400

    Implemented Backend for Getting Current Friends + Refactoring

--- services/roomService/src/CoveyTypes.ts
+++ services/roomService/src/CoveyTypes.ts
@@ -1,14 +1,29 @@
 export type Direction = 'front' | 'back' | 'left' | 'right';
+
 export type UserLocation = {
   x: number;
   y: number;
   rotation: Direction;
   moving: boolean;
 };
+
 export type CoveyTownList = {
   friendlyName: string;
   coveyTownID: string;
   currentOccupancy: number;
   maximumOccupancy: number;
 }[];
+
 export type FriendRequestAction = 'accept' | 'deny';
+
+export type FriendsListActionName = 'getPendingFriendRequests' | 'getCurrentListOfFriends';
+
+export type FriendsListAction = {
+  actionName: FriendsListActionName;
+  errorMessage: string;
+};
+
+export type FriendsListResponse = {
+  success: boolean;
+  response: string[];
+};
